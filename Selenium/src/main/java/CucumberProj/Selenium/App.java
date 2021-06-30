83f208a2a848f5d03675a89ed23894c43128f711
JijiAutomation <jijibj01@gmail.com>
2021-06-30 08:03:59 +0530

    Changes made by suguna

--- Selenium/src/main/java/CucumberProj/Selenium/App.java
+++ Selenium/src/main/java/CucumberProj/Selenium/App.java
@@ -10,10 +10,13 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        String args1, args2;
+        String args1, args2, args3, args4;
         
         args1 = "Hello World";
         
         args2 = "Changes made in new folder";
+        
+        args3 = "Changes new1";
+        args4 = "Changes new2";
     }
 }
